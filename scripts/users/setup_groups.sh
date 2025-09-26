1b4040f4ea46f952a900b3e8cfae4975da567b64
CallumH25 <CallumHipkiss2@outlook.com>
2025-09-26 16:21:09 +0000

    Wrote a script to create groups, add users into groups and third day documentation.

---
+++ scripts/users/setup_groups.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+echo "This script can add groups or add users to groups, follow along with the prompts."
+
+echo "Do you want to create a group? (y/n)"
+
+read decision
+
+if [ "$decision" = "y" ]; then
+
+        echo "Please choose a group name to create."
+
+        read groupname
+
+        groupadd "$groupname"
+
+fi
+
+echo "To add users to a group, please enter the users name now."
+
+read user
+
+echo "To add "$user" to a group, please enter the groups name now."
+
+read group
+
+usermod -g "$group" "$user"
+
+echo "The user should now be in the correct group."
